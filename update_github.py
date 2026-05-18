4e41c99cb3f93328d4fb695672199fa2c79c7309
carvalauto <xiejiahua96@gmail.com>
2026-05-19 01:09:02 +0800

    Add GitHub updater script

--- update_github.py
+++ update_github.py
@@ -3,16 +3,16 @@ import base64
 import os
 import urllib.request
 
-TOKEN = os.environ['GITHUB_TOKEN']
-REPO = os.environ['GITHUB_REPOSITORY']
-BASE_URL = f'https://api.github.com/repos/{REPO}'
+TOKEN = os.environ.get('GITHUB_TOKEN')
+REPO = os.environ.get('GITHUB_REPOSITORY')
+BASE_URL = 'https://api.github.com/repos/' + REPO
 
 with open('sitemap.xml', 'r') as f:
     sitemap = f.read()
 
 req = urllib.request.Request(
-    f'{BASE_URL}/contents/sitemap.xml',
-    headers={'Authorization': f'token {TOKEN}'}
+    BASE_URL + '/contents/sitemap.xml',
+    headers={'Authorization': 'token ' + TOKEN}
 )
 sha = None
 try:
@@ -30,9 +30,9 @@ if sha:
     data['sha'] = sha
 
 req = urllib.request.Request(
-    f'{BASE_URL}/contents/sitemap.xml',
+    BASE_URL + '/contents/sitemap.xml',
     data=json.dumps(data).encode(),
-    headers={'Authorization': f'token {TOKEN}'},
+    headers={'Authorization': 'token ' + TOKEN},
     method='PUT'
 )
 
@@ -40,5 +40,3 @@ with urllib.request.urlopen(req) as resp:
     result = json.loads(resp.read())
     if 'commit' in result:
         print('sitemap.xml 更新成功!')
-    else:
-        print('更新失败')
