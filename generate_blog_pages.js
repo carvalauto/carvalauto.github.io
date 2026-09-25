19f1d5aa2d0a46d04a240060da262e24425a3611
carvalauto <xiejiahua96@gmail.com>
2026-09-26 03:34:15 +0800

    fix(mobile): remove maximum-scale + enlarge small fonts for mobile-friendly

--- generate_blog_pages.js
+++ generate_blog_pages.js
@@ -110,7 +110,7 @@ for (const a of blogs) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${esc(title)}</title>
     <meta name="description" content="${esc(desc)}">
     <meta name="keywords" content="${esc(keywords)}">
